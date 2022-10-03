4746afa3572fc1de5ce09b974426d74081e1f8ac
LealSilverio <saralealsilverio@gmail.com>
2022-10-03 15:26:31 -0600

    chamber of commerce

--- lesson3/js/main.js
+++ lesson3/js/main.js
@@ -1,4 +1,7 @@
-const today = new Date();
+// this is the most up to date version of defer. Instead of defer, I do this
+window.addEventListener('load', () => {
+    const today = new Date();
 
-document.querySelector("footer div span").textContent = today.getFullYear();
-document.getElementById("dateUpdate").textContent = document.lastModified;
+    document.querySelector("footer div span").textContent = today.getFullYear();
+    document.getElementById("dateUpdate").textContent = document.lastModified;
+})
