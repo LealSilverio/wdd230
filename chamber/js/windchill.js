c7c4bb142f46dd9a5763584c6609dc7723eee100
LealSilverio <saralealsilverio@gmail.com>
2022-11-26 22:32:08 -0700

    chanber update

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,23 +1,32 @@
-window.addEventListener('load', (today) => {
-    
-    let temp = document.querySelector('.temperature').textContent;
-    let windSpeed = document.querySelector('.windS').textContent;
-    let windChill = document.querySelector(".windC").textContent;
-    Number(temp);
-    Number(windSpeed);
 
-    console.log(temp, windSpeed);
-
-    if (temp <= 50 && windSpeed > 3.0) { // this line doesnt
-        windChill = calculateWind(temp, windSpeed);
-        console.log(windChill);
-    } 
-    else {  
-        windChill = 'N/A';
-    }
+// api.openweathermap.org/data/2.5/forecast?id=524901&appid={ec4f576b367da3067cc0909fb24b1cb2};
+ 
+const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={ec4f576b367da3067cc0909fb24b1cb2}";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
 });
+
+
+
+let temp = document.querySelector('.temperature').textContent;
+let windSpeed = document.querySelector('.windS').textContent;
+Number(temp);
+Number(windSpeed);
+
+console.log(temp, windSpeed);
+
+if (temp <= 50 && windSpeed > 3.0) {
+    document.querySelector(".windC").innerHTML = calculateWind(temp, windSpeed);
+    console.log("here")
+} 
+else {  
+    console.log('not here')
+    document.querySelector(".windC").innerHTML = 'N/A';
+}
 
 function calculateWind(temp, windSpeed) {
     let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windSpeed,0.16)) + (0.4275*temp*Math.pow(windSpeed,0.16)));
-    console.log(chill);
+    return chill;
 }
