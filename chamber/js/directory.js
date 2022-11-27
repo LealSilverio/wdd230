67c756ee9118c0f8b5ecda02686bcdb6a6ae9a40
LealSilverio <saralealsilverio@gmail.com>
2022-11-26 19:21:10 -0700

    directory updated

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,3 +1,5 @@
+const gridButton = document.querySelector('#grid');
+const listButton = document.querySelector('#list');
 const requestURL = "./js/data.json"; 
 
 fetch(requestURL)
@@ -7,10 +9,11 @@ fetch(requestURL)
 .then(function (jsonObject) {
   console.table(jsonObject); 
   let business = jsonObject['businesses'];
-  business.forEach(displaybusiness);
+  business.forEach(displaygrid);
+  business.forEach(displaylist);
 });
 
-function displaybusiness(business) {  
+function displaygrid(business) {  
   let card = document.createElement('section');
 
   let img = document.createElement('img');
@@ -37,18 +40,38 @@ function displaybusiness(business) {
 
   document.querySelector('div.cards').appendChild(card);
 }
+function displaylist(business) {  
+  let card = document.createElement('section');
 
-// buttons
-const gridButton = document.querySelector('#grid');
-const listButton = document.querySelector('#list');
+  let h2 = document.createElement('h2'); 
+  h2.textContent = business.name; 
+  
+  let p1 = document.createElement('p');
+  p1.textContent = business.address;
+  
+  let p2 = document.createElement('p');
+  p2.textContent = business.phone;
 
+  let web = document.createElement('a');
+  web.textContent = business.name;
+  web.setAttribute('href', business.website);
 
+  card.appendChild(h2);
+  card.appendChild(p1);
+  card.appendChild(p2);
+  card.appendChild(web);
+
+  document.querySelector('div.list').appendChild(card);
+}
+
+// event listener
 gridButton.addEventListener('click', () => {
     document.getElementById('grid').classList.toggle('open');
-    console.log('it works')
+    document.querySelector('.cards').classList.toggle('open');
+    console.log("here");
 })
 
 listButton.addEventListener('click', () => {
   document.getElementById('list').classList.toggle('open');
-  console.log('it works')
+  document.querySelector('.list').classList.toggle('open');
 })
