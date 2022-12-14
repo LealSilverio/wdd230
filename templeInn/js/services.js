9b07a4290c5bbadf0a356e83eb6c761aa61fe212
LealSilverio <saralealsilverio@gmail.com>
2022-12-13 20:13:03 -0700

    final before corrections

--- templeInn/js/services.js
+++ templeInn/js/services.js
@@ -1,16 +1,87 @@
-// window.localStorage.setTime("last-visit", new Date('09/25/2022'));
+const gridButton = document.querySelector('#grid');
+const requestURL = "./js/services.json"; 
 
-const visit = document.querySelector("#visit-messafe");
+fetch(requestURL)
+.then(function (response) {
+  return response.json();
+})
+.then(function (jsonObject) {
+  console.table(jsonObject); 
+  let content = jsonObject['content'];
+  content.forEach(displaygridS);
+  content.forEach(displaygridA);
+});
 
-let visitMessage = "This is your first visit to the page.";
-let todayDate = new Date();
+function displaygridS(content) {  
+    let card = document.createElement('section');
+  
+    let h2 = document.createElement('h2'); 
+    h2.textContent = "Services"; 
+    
+    let p1 = document.createElement('p');
+    p1.textContent = content.services[0];
 
-let lastVisitString = window.localStorage.getItem("last-visit");
-if (lastVisitString != null) {
-    let lastVisitDate = new Date(lastVisitString);
-    let dateDifference = Math.floor((todayDate.getTime() - lastVisitDate.getTime()) / (24 * 60 * 1000));
-    visitMessage = `You last visited the page ${dateDifference} days ago.`;
+    let p2 = document.createElement('p');
+    p2.textContent = content.services[1];
+
+    let p3 = document.createElement('p');
+    p3.textContent = content.services[2];
+
+    let p4 = document.createElement('p');
+    p4.textContent = content.services[3];
+
+    let p5 = document.createElement('p');
+    p5.textContent = content.services[4];
+
+    let p6 = document.createElement('p');
+    p6.textContent = content.services[5];
+
+    card.appendChild(h2);
+    card.appendChild(p1);
+    card.appendChild(p2);
+    card.appendChild(p3);
+    card.appendChild(p4);
+    card.appendChild(p5);
+    card.appendChild(p6);
+
+    document.querySelector('div.services').appendChild(card);
 }
 
-visit.textContent = visitMessage;
-window.localStorage.setItem("last-visit", todayDate.toString());
+function displaygridA(content) {  
+    let card = document.createElement('section');
+  
+    let h2 = document.createElement('h2'); 
+    h2.textContent = "Free Amenities"; 
+    
+    let p1 = document.createElement('p');
+    p1.textContent = content.amenities[0];
+
+    let p2 = document.createElement('p');
+    p2.textContent = content.amenities[1];
+
+    let p3 = document.createElement('p');
+    p3.textContent = content.amenities[2];
+
+    let p4 = document.createElement('p');
+    p4.textContent = content.amenities[3];
+
+    let p5 = document.createElement('p');
+    p5.textContent = content.amenities[4];
+
+    let p6 = document.createElement('p');
+    p6.textContent = content.amenities[5];
+
+    let p7 = document.createElement('p');
+    p7.textContent = content.amenities[6];
+
+    card.appendChild(h2);
+    card.appendChild(p1);
+    card.appendChild(p2);
+    card.appendChild(p3);
+    card.appendChild(p4);
+    card.appendChild(p5);
+    card.appendChild(p6);
+    card.appendChild(p7);
+
+    document.querySelector('div.amenities').appendChild(card);
+}
--- templeInn/js/temple.js
+++ templeInn/js/temple.js
@@ -13,7 +13,7 @@ fetch(requestURL)
 
 function displaygrid(temple) {  
   let card = document.createElement('section');
-
+  
   let img = document.createElement('img');
   img.setAttribute('src', temple.image);
 
@@ -26,35 +26,99 @@ function displaygrid(temple) {
   let p2 = document.createElement('p');
   p2.textContent = temple.phone;
 
-  let div = document.createElement('div')
-
   let h3 = document.createElement('h3');
   h3.textContent = "Services:";
 
-  let li = document.createElement('li');
-  li.textContent = temple.services.forEach(split(" "));
+  let li1 = document.createElement('li');
+  li1.textContent = temple.services[0];
+
+  let li2 = document.createElement('li');
+  li2.textContent = temple.services[1];
+
+  let li3 = document.createElement('li');
+  li3.textContent = temple.services[2];
+
+  let li4 = document.createElement('li');
+  li4.textContent = temple.services[3];
   
-  let h4 = document.createElement('h3')
+  let h4 = document.createElement('h3');
   h4.textContent = "History";
 
   let hist = document.createElement('p');
   hist.textContent = temple.history.join();
 
+  // let h5 = document.createElement('h3');
+  // h5.textContent = "Temple Closures";
+
+  // let li01 = document.createElement('li');
+  // li01.textContent = temple.temple_closure[0]; 
+  
+  // let li02 = document.createElement('li');
+  // li02.textContent = temple.temple_closure[1]; 
+  
+  // let li03 = document.createElement('li');
+  // li03.textContent = temple.temple_closure[2]; 
+  
+  // let li04 = document.createElement('li');
+  // li04.textContent = temple.temple_closure[3]; 
+  
+  // let li05 = document.createElement('li');
+  // li05.textContent = temple.temple_closure[4]; 
+  
+  // let li06 = document.createElement('li');
+  // li06.textContent = temple.temple_closure[5]; 
+  
+  // let li07 = document.createElement('li');
+  // li07.textContent = temple.temple_closure[6]; 
+  
+  // let li08 = document.createElement('li');
+  // li08.textContent = temple.temple_closure[7]; 
+  
+  // let li09 = document.createElement('li');
+  // li09.textContent = temple.temple_closure[8]; 
+  
+  // let li10 = document.createElement('li');
+  // li10.textContent = temple.temple_closure[9]; 
+  
+  // let li11 = document.createElement('li');
+  // li11.textContent = temple.temple_closure[10]; 
+  
+  // let li12 = document.createElement('li');
+  // li12.textContent = temple.temple_closure[11]; 
+
+
   card.appendChild(img);
   card.appendChild(h2);
   card.appendChild(p1);
   card.appendChild(p2);
-  card.appendChild(div);
-  card.div.appendChild(h3);
-  card.div.appendChild(li);
-  card.div.appendChild(hist);
+  
+  card.appendChild(h3);
+  card.appendChild(li1);
+  card.appendChild(li2);
+  card.appendChild(li3);
+  card.appendChild(li4);
+  card.appendChild(h4);
+  card.appendChild(hist);
+  // card.appendChild(h5);
+  // card.appendChild(li01);
+  // card.appendChild(li02);
+  // card.appendChild(li03);
+  // card.appendChild(li04);
+  // card.appendChild(li05);
+  // card.appendChild(li06);
+  // card.appendChild(li07);
+  // card.appendChild(li08);
+  // card.appendChild(li09);
+  // card.appendChild(li10);
+  // card.appendChild(li11);
+  // card.appendChild(li12);
 
   document.querySelector('div.cards').appendChild(card);
 }
 
 // see more button
 function seeMore() {
-  var dots = document.getElementById("dots");
+  var x = 1;
   var moreText = document.getElementById("more");
   var btnText = document.getElementById("myBtn");
 
@@ -65,6 +129,18 @@ function seeMore() {
   } else {
     dots.style.display = "none";
     btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+function seeMore() {
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (moreText.style.display === "none") {
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    btnText.innerHTML = "Read less"; 
     moreText.style.display = "inline";
   }
 }
